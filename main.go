8cf95fc7645ceb4c5ffc207ba394c4dacd76905b
Zi Hong <74897601+sk4rm@users.noreply.github.com>
2024-11-15 13:44:43 -0800

    Add background image

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2/colorm"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
+
+var bg *ebiten.Image
+
+func init() {
+	var err error
+	bg, _, err = ebitenutil.NewImageFromFile("img/background.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Game struct{}
 
@@ -14,16 +26,26 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
+	// Draw background image
+	op := &colorm.DrawImageOptions{}
+	size := bg.Bounds().Size()
+	op.GeoM.Translate(-float64(size.X/2), -float64(size.Y/2))
+	op.GeoM.Scale(0.667, 0.667)
+	c := colorm.ColorM{}
+	c.ChangeHSV(0, 1, 24)
+	colorm.DrawImage(screen, bg, c, op)
+
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("%v", ebiten.Monitor().DeviceScaleFactor()))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	s := ebiten.Monitor().DeviceScaleFactor()
+	return int(320 * s), int(240 * s)
 }
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowTitle("Connect Four")
 	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
